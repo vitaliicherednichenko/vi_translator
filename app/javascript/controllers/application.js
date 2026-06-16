3c0cf037502a91073f36723120e3674ffd60ecbc
Vitaliy Cherednichenko <vitaliycherednichenko@Vitaliys-MacBook-Pro.local>
2026-06-16 10:31:33 +0200

    Add google analytics

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,14 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.addEventListener("turbo:load", (event) => {
+    let googleId = document.querySelector("meta[name='google-analytics-id']").content
+    if (googleId) {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', googleId);
+    }
+})
